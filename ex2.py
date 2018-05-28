fe6a886a8ad3f3f61bb8d504b484b8df69cc16fe
michalmaj90 <michalmaj90@gmail.com>
2018-05-28 16:07:11 +0200

    Evolved exercise no 2

--- ex2.py
+++ ex2.py
@@ -1,7 +1,9 @@
 '''Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.'''
 
 num = int(input("Give me a number: "))
-if num % 2 == 0:
+if num % 4 == 0:
+    print('Your number is a multiple of 4!')
+elif num % 2 == 0:
     print('Your number is even!')
 else:
     print('Your number is odd!')
