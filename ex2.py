fb6006a647782a462694bfb0d88f60fbf83a76ba
michalmaj90 <michalmaj90@gmail.com>
2018-05-28 15:55:57 +0200

    Exercise no 2

---
+++ ex2.py
@@ -0,0 +1,7 @@
+'''Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.'''
+
+num = int(input("Give me a number: "))
+if num % 2 == 0:
+    print('Your number is even!')
+else:
+    print('Your number is odd!')
